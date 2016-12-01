3b9df3980417186339d5de0fbc49890bf4564e9d
BuildTools <unconfigured@null.spigotmc.org>
2016-12-01 13:58:51 -0800

    Uploading Files

---
+++ start.sh
@@ -0,0 +1,11 @@
+@echo off
+title Minecraft Server Console [Bukkit 1.8.8] @xSplayd
+color 0e
+cls
+java -Xmx1100M -jar craftbukkit.jar -o true
+echo.
+echo ------------------------------------------------------------------
+echo             //Press the any key to end console //
+echo ------------------------------------------------------------------
+PAUSE>nul
+exit()
